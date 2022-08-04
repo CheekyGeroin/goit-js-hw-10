8a8d187763bf7a67ce0d252c46849e6985f6d6ab
Danylo Altukhov <dan.97198@gmail.com>
2022-08-04 21:52:18 +0300

    cделал разметку для страны

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import cardTemplate from './templates/card-template.hbs';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -14,17 +15,38 @@ refs.input.addEventListener(
 );
 
 function onFetchCountries() {
-  const name = refs.input.value;
-  fetchCountries(name);
+  const name = refs.input.value.trim().toLowerCase();
+  name.trim();
+  if (name != ' ') {
+    fetchCountries(name);
+  } else {
+    Notiflix.Notify.warning('Write name of country!');
+  }
 }
 
 function fetchCountries(name) {
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(r => r.json())
-    .then(country => {
-      console.log(country);
-      //   const markUp = cardTemplate(country);
-      //   console.log(markUp);
+  fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,languages,population,flags`
+  )
+    .then(r => {
+      return r.json();
     })
-    .catch(error => console.log(error));
+    .then(createTemplate)
+    .catch(catchError);
+}
+
+function createTemplate(country) {
+  console.log(country);
+  if (country > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (country >= 2 && country <= 10) {
+  } else {
+    const markUp = cardTemplate(country);
+    refs.cardInfo.innerHTML = markUp;
+  }
+}
+function catchError(error) {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 }
