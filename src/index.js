7c146803d7965542841f3cc7704505580684012f
Danylo Altukhov <dan.97198@gmail.com>
2022-08-04 14:43:10 +0300

    добавил файлы и сделал поиск страны

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import cardTemplate from './templates/card-template.hbs';
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  cardInfo: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener(
+  'input',
+  debounce(onFetchCountries, DEBOUNCE_DELAY)
+);
+
+function onFetchCountries() {
+  const name = refs.input.value;
+  fetchCountries(name);
+}
+
+function fetchCountries(name) {
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(r => r.json())
+    .then(country => {
+      console.log(country);
+      //   const markUp = cardTemplate(country);
+      //   console.log(markUp);
+    })
+    .catch(error => console.log(error));
+}
